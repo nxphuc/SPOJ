bf8daab4edd0363b627ae7b17babb64d81059d00
nxphuc <nguyenxuanphuc95@gmail.com>
2014-08-10 19:11:12 +0700

    C++
    C++

---
+++ ACT.cpp
@@ -0,0 +1,18 @@
+/*
+LINK: http://www.spoj.com/problems/ACT/
+TASK: ad-hoc
+*/
+
+#include <cstdio>
+#include <cstring>
+
+int main(){
+	char str[50005];
+	int t, n;
+	scanf("%d", &t);
+	while(t--){
+		scanf("%d %s", &n, str);
+		printf("%c\n", str[strlen(str)-1]);
+	}
+	return 0;
+}
