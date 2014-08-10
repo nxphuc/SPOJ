5d9613fc585b3dd386e3675e1dbd29e6be2b6798
nxphuc <nguyenxuanphuc95@gmail.com>
2014-08-10 18:49:32 +0700

    C++
    C++

---
+++ ACPC10A.cpp
@@ -0,0 +1,17 @@
+/*
+LINK: http://www.spoj.com/problems/ACPC10A/
+TASK: ad-hoc
+*/
+
+#include <cstdio>
+
+int main(){
+	int a, b, c;
+	while(scanf("%d%d%d", &a, &b, &c) == 3 && (a | b | c)) {
+		if(b - a == c - b)
+		  printf("AP %d\n", c + c - b);
+		else
+		  printf("GP %d\n", c * c / b);
+	}
+	return 0;
+}
