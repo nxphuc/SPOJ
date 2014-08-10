db4821139ac0622407ac2a694a692b24e4d87fd4
nxphuc <nguyenxuanphuc95@gmail.com>
2014-08-10 21:29:32 +0700

    C++
    C++

---
+++ AMR10F.cpp
@@ -0,0 +1,16 @@
+/*
+LINK: http://www.spoj.com/problems/AMR10F/
+TASK: math
+*/
+
+#include <cstdio>
+
+int main() {
+	int t, n, a, d;
+	scanf("%d", &t);
+	while(t--) {
+		scanf("%d%d%d", &n, &a, &d);
+		printf("%d\n", n * (2 * a + (n - 1) * d) / 2);
+	}
+	return 0;
+}
