59b433041f495e3d2838b3d4e9eaebceceee226d
nxphuc <nguyenxuanphuc95@gmail.com>
2014-08-10 21:22:53 +0700

    C++
    C++

---
+++ ALCHE.cpp
@@ -0,0 +1,18 @@
+/*
+LINK: http://www.spoj.com/problems/ALCHE/
+TASK: ad-hoc
+*/
+
+#include <cstdio>
+
+int main() {
+	int L, W;
+	while(scanf("%d%d", &L, &W) == 2) {
+		if(L == -1 && W == -1) break;
+		if(37 * L == 1000 * W)
+		  puts("Y");
+		else
+		  puts("N");
+	}
+	return 0;
+}
