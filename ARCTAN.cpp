617374a2b049ec4cd97d3d98da059d0e6cede29e
nxphuc <nguyenxuanphuc95@gmail.com>
2014-08-11 22:21:22 +0700

    C++
    C++

---
+++ ARCTAN.cpp
@@ -0,0 +1,10 @@
+/*
+LINK: http://www.spoj.com/problems/ARCTAN/
+TASK: math
+NOTE: trickly: "Source limit: 256B"
+*/
+
+#include<cstdio>
+#include<algorithm>
+int main(){long long a,c,t,b;scanf("%lld",&t);while(t--){scanf("%lld",&a);c=1;c <<= 60;
+for(b=a+1;b <= 2*a+1;b++)if((a*b + 1) % (b-a) == 0) c=std::min(c, b+(a*b+1) / (b-a)); printf("%lld\n", c);} return 0; }
