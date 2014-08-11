c02751d43c652f0d6884564d8a573159c7e861a8
nxphuc <nguyenxuanphuc95@gmail.com>
2014-08-11 21:25:15 +0700

    C++
    C++

---
+++ ANARC09B.cpp
@@ -0,0 +1,26 @@
+/*
+LINK: http://www.spoj.com/problems/ANARC09B/
+TASK: math
+*/
+
+#include <cstdio>
+
+#define ll long long
+
+ll gcd(ll a, ll b ){
+	if(b) return gcd(b, a % b);
+	return a;
+}
+
+ll lcm(ll a, ll b){
+	return a / gcd(a, b) * b;
+}
+
+int main(){
+	int a, b;
+	while(scanf("%d%d", &a, &b) && a) {
+		ll LCM = lcm(a, b);
+		printf("%lld\n", (LCM / a) * (LCM / b));
+	}
+	return 0;
+}
