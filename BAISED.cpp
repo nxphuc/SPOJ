0d260e1e23c11f504ac129275d65d8406355ff71
nxphuc <nguyenxuanphuc95@gmail.com>
2014-08-12 21:29:01 +0700

    C++
    C++

---
+++ BAISED.cpp
@@ -0,0 +1,29 @@
+/*
+LINK: http://www.spoj.com/problems/BAISED/
+TASK: sorting
+*/
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+#define ll long long
+int a[100001];
+char name[100001];
+
+int main(){
+	int n, i, t;
+	ll ans;
+
+	scanf("%d", &t);
+	while(t--){
+		scanf("%d", &n);
+		for(i = 0; i < n; i++)
+			scanf("%s %d", name, &a[i]);
+		sort(a, a + n);
+		ans = 0;
+		for(i = 0; i < n; i++)
+			ans += abs(a[i] - (i + 1));
+		printf("%lld\n", ans);
+	}
+	return 0;
+}
