589126ed6469e70187a75fffb814ec169eb4386a
nxphuc <nguyenxuanphuc95@gmail.com>
2014-08-13 20:22:03 +0700

    C++
    C++

---
+++ BINSTIRL.cpp
@@ -0,0 +1,16 @@
+/*
+LINK: http://www.spoj.com/problems/BINSTIRL/
+TASK: math
+*/
+
+#include <cstdio>
+
+int main(){
+	int t, n, m;
+	scanf("%d", &t);
+	while(t--){
+		scanf("%d%d", &n, &m);
+		printf("%d\n", !((n - m) & ((m - 1) >> 1)));
+	}
+	return 0;
+}
